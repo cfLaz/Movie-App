3915b7adf3792bd14f43bd5fb053320a4e7ca199
Laz <jankoviclaz@gmail.com>
2021-07-25 20:54:01 +0200

    pulling more movies and removing movies from previous search

--- app.js
+++ app.js
@@ -2,23 +2,47 @@ const searchMovie = document.getElementById('searchMovie');
 
 let fetchRequest = async function(movie) {
 
-  const response = await fetch(`http://www.omdbapi.com/?t=${movie}&apikey=6920da43`);
-  console.log(response);
+  const response = await fetch(`http://www.omdbapi.com/?s=${movie}&apikey=6920da43`);
+  //console.table(response);
 
   let data = await response.json();
   console.log(data);
-  return data;
+  return data.Search;
 }
+
+let showMovieList = (movies) =>{
+  
+/*   if(document.querySelectorAll('div.movieInList')) {
+    let el = document.querySelectorAll('div.movieInList');
+    el.parentElement.removeChild(el);
+  }
+ */let elements = document.querySelectorAll('div.movieInList');
+  if(elements.length>0)  
+    for(el of elements){
+      el.remove();
+    }
+
+  const list = document.getElementById('searchList'); 
+  for(movie of movies){
+    let content = document.createElement('div')
+    content.setAttribute('class', 'movieInList');
+    content.innerHTML=`
+        <img src=${movie.Poster} alt=${movie.Title}>
+        <p>${movie.Title}</p>
+        <p>${movie.Year}</p>
+    `
+    list.append(content);
+  }
+};
 
 async function SearchAndDisplay(e){
   e.preventDefault(); //prevents the form from autosubmitting
   //console.log(e.target);
-  let movie = document.getElementById('movie').value;
-  console.log(movie);
+  let movieName = document.getElementById('movieName').value;
+  console.log(movieName);
 
-  let movieData = await fetchRequest(movie);
-
-  
+  let moviesData = await fetchRequest(movieName);
+  showMovieList(moviesData);
 }
 
 searchMovie.addEventListener('submit', (e) => SearchAndDisplay(e));
