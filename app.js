6b6ecf47a8788115d804048ff8b0247149f2d278
Laz <jankoviclaz@gmail.com>
2021-07-27 00:07:00 +0200

    Handling the additon/removal of movies to/from myList

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ let myList = document.getElementById('myList');
 let movieInfo = document.getElementById('movieInfo');
 
 const searchMovieForm = document.getElementById('searchMovie');
-let toggleMovieButton;
-let removeMovieButton;
+//let toggleMovieButton;
+//let removeMovieButton;
 
 showMovieInfoFromLocalStorage = (movie) => {
-  
+  console.log('showMovieInfoFromLocalStorage fired');
   let toggleMovieButton;
 
   movieInfo.style.display='flex';
@@ -23,79 +23,88 @@ showMovieInfoFromLocalStorage = (movie) => {
     <p class='movieInfoElement'>Director: ${movie.Director}</p>
     <p class='movieInfoElement'>Actors: ${movie.Actors}</p>
     <p class='movieInfoElement'>Plot: <br> ${movie.Plot}</p>
-    <button class='movieInfoElement' id='addOrRemoveMovie'>Add to my list</button>   
+    <button class='movieInfoElement' id='addOrRemoveMovie'>Remove from my list</button>   
   `;
   toggleMovieButton = document.getElementById('addOrRemoveMovie');
   toggleMovieButton.addEventListener('click', (e)=> toggleMovie(e));
 
   if(localStorage.getItem(movieInfo.children[0].innerText)){
+    console.log('movie is bookmarked')
     toggleMovieButton.textContent = 'Remove from my list';
   }
 }
 
-/* 
-let removeMovie= (content) => {
-  content.remove();
+
+let addMovieToMyList =(movie) =>{
+  
+  let content = document.createElement('div');
+    content.setAttribute('class', 'movieInMyList');
+    content.setAttribute('id', movie.Title); //for easier deleting from movieInfo section
+    let removeMovieBtn = document.createElement('button');
+    removeMovieBtn.setAttribute('class', 'removeMovie');
+    removeMovieBtn.textContent='Remove';
+
+    content.innerHTML=`
+        <img src=${movie.Poster} alt=${movie.Title}>
+        <p>${movie.Title}</p>
+        <p>${movie.Year}</p>
+    `;
+    content.append(removeMovieBtn);
+    
+    removeMovieBtn.addEventListener('click', (e)=> {
+      e.stopPropagation();
+      content.remove()
+      toggleMovie(e);
+    });
+    content.addEventListener('click', () => showMovieInfoFromLocalStorage(movie));
+    myList.append(content);
 }
- */
+
 let showMyMovies=()=> {
   let movies = [];
   let keys = Object.keys(localStorage);
-
-  let elements = document.querySelectorAll('div.movieInList');
+  //let removeMovieButton;
+  let elements = document.querySelectorAll('div.movieInMyList');
   
   if(elements.length>0)  
     for(el of elements){
       el.remove();
     }
-
   for(key of keys){
     movies.push(JSON.parse(localStorage.getItem(key)) );
   }
-  //let removeMovieButton;
 
   for(let movie of movies){
-    let content = document.createElement('div');
-    content.setAttribute('class', 'movieInList');
-    content.innerHTML=`
-        <img src=${movie.Poster} alt=${movie.Title}>
-        <p>${movie.Title}</p>
-        <p>${movie.Year}</p>
-        <button id='removeMovie'>Remove</button>
-    `
-
-    /* removeMovieButton = document.getElementById('removeMovie');
-    removeMovieButton.addEventListener('click', ()=> content.remove()); */
-
-    content.addEventListener('click', () => showMovieInfoFromLocalStorage(movie));
-    myList.append(content);
+    addMovieToMyList(movie);
   } 
 }
 
 let toggleList = (e) =>{
   //console.log(e.target.id); // i need .target.id
   if(e.target.id === 'myListButton'){
-    searchList.style.display = 'none';
+    searchList.style.visibility = 'hidden'; //so that search result stays when we return (don't have to search again for the same movie title)
     myList.style.display = 'flex';
 
     myListBtn.disabled=true;
     searchListBtn.disabled=false;
     showMyMovies();
+    movieInfo.style.display='none';       
   }
   else{
     myList.style.display = 'none';
-    searchList.style.display='flex';
+    searchList.style.visibility='visible';
 
     searchListBtn.disabled=true;
     myListBtn.disabled=false;
+    movieInfo.style.display='none';   
   }
 
 }
 
 let toggleMovie =(e) => {
-  
+  console.log('toggleMovie fired with button: '+e.target.textContent);
   if(e.target.textContent==='Add to my list'){
-    console.log(movieInfo.children);
+    //console.log(movieInfo.children);
     let info = movieInfo.children;
     
     let data = {
@@ -112,12 +121,27 @@ let toggleMovie =(e) => {
     //let retrievedData = localStorage.getItem(data.Title);
     //console.log(JSON.parse(retrievedData));
     e.target.textContent='Remove from my list';
+
+    addMovieToMyList(data);
+  }
+  else if (e.target.textContent==='Remove'){
+    if(movieInfo.children[0]){
+      let title = movieInfo.children[0].innerText;
+      //console.log(title);
+      localStorage.removeItem(title);
+  
+      let toggleMovieButton = document.getElementById('addOrRemoveMovie');
+      toggleMovieButton.textContent = 'Add to my list';
+    }
+
+   //movieInfo.style.display='none';    
   }
   else {
     let title = movieInfo.children[0].innerText;
     console.log(title);
     localStorage.removeItem(title);
     e.target.textContent='Add to my list';
+    document.getElementById(title).remove();
   };
 }
 
@@ -139,26 +163,17 @@ async function showMovieInfo(id){
     <button class='movieInfoElement' id='addOrRemoveMovie'>Add to my list</button>   
   `;
   toggleMovieButton = document.getElementById('addOrRemoveMovie');
-  toggleMovieButton.addEventListener('click', (e)=> removeMovie(e));
+  toggleMovieButton.addEventListener('click', (e)=> toggleMovie(e));
 
   if(localStorage.getItem(movieInfo.children[0].innerText)){
     toggleMovieButton.textContent = 'Remove from my list';
   }
 }
 
-let fetchRequest = async function(movie) {
-
-  const response = await fetch(`http://www.omdbapi.com/?s=${movie}&apikey=6920da43`);
-  //console.table(response);
-
-  let data = await response.json();
-  console.log(data);
-  return data.Search;
-}
 
 let showMoviesList = (movies) =>{
   
-  let elements = document.querySelectorAll('div.movieInList');
+  let elements = document.querySelectorAll('div.movieInSearchList');
   
   if(elements.length>0)  
     for(el of elements){
@@ -169,7 +184,7 @@ let showMoviesList = (movies) =>{
     let content = document.createElement('div')
     let id = movie.imdbID; 
 
-    content.setAttribute('class', 'movieInList');
+    content.setAttribute('class', 'movieInSearchList');
     content.innerHTML=`
         <img src=${movie.Poster} alt=${movie.Title}>
         <p>${movie.Title}</p>
@@ -181,6 +196,15 @@ let showMoviesList = (movies) =>{
 };
 
 
+let fetchRequest = async function(movie) {
+
+  const response = await fetch(`http://www.omdbapi.com/?s=${movie}&apikey=6920da43`);
+  //console.table(response);
+
+  let data = await response.json();
+  console.log(data);
+  return data.Search;
+}
 
 async function SearchAndDisplay(e){
   e.preventDefault(); //prevents the form from autosubmitting
